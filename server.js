e80397ad9b0db21f4d24bd6f4ead2a09ebf08589
Sam Smith <sam@example.com>
2020-09-02 09:56:45 +0200

    corrected MQ responsivity

--- server.js
+++ server.js
@@ -1,12 +1,17 @@
-const express = require('express')
+const express = require('express');
+const livereload = require("livereload");
+const connectLiveReload = require("connect-livereload");
 const server = express();
+const liveReloadServer = livereload.createServer();
 
+server.use(connectLiveReload());
 server.use(express.static("public", {
     "index": "signup.html"
 }));
+liveReloadServer.watch("public");
 
 const port = 5040;
 
 server.listen(port, function () {
-    console.log(`Server is listening to port:${port}`)
+    console.log(`Server is listening to port:${port}`);
 });
